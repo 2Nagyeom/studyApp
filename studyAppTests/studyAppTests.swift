1822563c25ff98b9188709ccaf4cdd714b99e723
2Nagyeom <artest1081@gmail.com>
2025-03-16 12:43:01 +0900

    Initial Commit

---
+++ studyAppTests/studyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  studyAppTests.swift
+//  studyAppTests
+//
+//  Created by NaGyeom Lee on 3/16/25.
+//
+
+import Testing
+@testable import studyApp
+
+struct studyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
