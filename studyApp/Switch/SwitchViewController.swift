edf93bd6f66303cdd408cefebfe6d80dd22a5dcd
2Nagyeom <artest1081@gmail.com>
2025-03-18 16:37:24 +0900

    스위치 기능 추가, 카운트값 추가

---
+++ studyApp/Switch/SwitchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayViewController.swift
+//  studyApp
+//
+//  Created by NaGyeom Lee on 3/18/25.
+//
+
+import UIKit
+
+class PlayViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
