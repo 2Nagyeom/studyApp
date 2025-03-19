ef83ddefb6632a69babdea0c7039724b375d50df
2Nagyeom <artest1081@gmail.com>
2025-03-19 18:49:14 +0900

    수정완료

--- studyApp/Calender/CalenderViewController.swift
+++ studyApp/Calender/CalenderViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class CalenderViewController: UIViewController {
 
+    @IBOutlet weak var curTimer: UILabel!
+    @IBOutlet weak var picTimer: UILabel!
+    
+    // 선택한 날짜를 저장할 변수
+    var pickedDate: Date?
+    // 현재 시간을 주기적으로 업데이트할 타이머
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 타이머를 이용해 매초 현재 시간을 업데이트
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(updateCurrentTime),
+                                     userInfo: nil,
+                                     repeats: true)
+        updateCurrentTime() // 초기 업데이트
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // DatePicker 액션: 선택한 날짜를 저장하고 레이블 업데이트
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        pickedDate = sender.date
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 MM월 dd일 HH시 mm분 EEE요일"
+        picTimer.text = "픽한시간 : " + formatter.string(from: sender.date)
+        
+        // 선택 후 즉시 현재 시간과 비교해 배경색 변경
+        checkTimeAndUpdateBackground()
     }
-    */
-
+    
+    // 현재 시간을 업데이트하는 메서드 (타이머가 호출)
+    @objc func updateCurrentTime() {
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 MM월 dd일 HH시 mm분 EEE요일"
+        curTimer.text = "현재시간 : " + formatter.string(from: now)
+        
+        // 업데이트 될 때마다 선택한 시간과 비교
+        checkTimeAndUpdateBackground()
+    }
+    
+    // 현재 시간과 선택한 시간이 같은지 비교하여 배경색을 변경하는 메서드
+    func checkTimeAndUpdateBackground() {
+        // 선택한 시간이 없으면 기본 배경색 유지
+        guard let picked = pickedDate else {
+            self.view.backgroundColor = UIColor.white
+            return
+        }
+        
+        let now = Date()
+        let calendar = Calendar.current
+        // 분 단위로 비교 (초단위까지 비교하면 거의 매번 달라짐)
+        if calendar.compare(now, to: picked, toGranularity: .minute) == .orderedSame {
+            // 현재 시간과 선택한 시간이 같은 경우, 배경색 변경
+            self.view.backgroundColor = UIColor.green
+        } else {
+            self.view.backgroundColor = UIColor.white
+        }
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
 }
+
--- studyApp/Switch/SwitchViewController.swift
+++ studyApp/Switch/SwitchViewController.swift
@@ -1,13 +1,6 @@
-//
-//  PlayViewController.swift
-//  studyApp
-//
-//  Created by NaGyeom Lee on 3/18/25.
-//
-
 import UIKit
 
-class SwitchViewController: UIViewController {
+class SwitchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     var isZoom = false
     var isBgOn: UIImage?
     var isBgOff: UIImage?
@@ -15,16 +8,39 @@ class SwitchViewController: UIViewController {
     var inputBtn = 0
     var inputSwitch = 0
     
+    // pickerView 변수 추가
+    let MAX_ARRAY_NUM = 8
+    let PICKER_VIEW_COLUM = 1
+    let PICKERVIEW_HEIGHT : CGFloat = 66
+    
+    var imageArray1 = [UIImage?]()
+    var imageArray2 = [UIImage?]()
+    
+    var gromitArray = ["gromit1.jpg", "gromit2.jpg", "gromit3.jpg", "gromit4.jpg", "gromit5.jpg", "gromit6.jpg", "gromit7.jpg", "gromit8.jpg", ]
+    var feathersArray = ["feathers1.jpg", "feathers2.jpg", "feathers3.jpg", "feathers4.jpg", "feathers5.jpg", "feathers6.jpg", "feathers7.jpg", "feathers8.jpg", ]
+    
     @IBOutlet var imgView: UIImageView!
     @IBOutlet weak var isAdd: UIButton!
     @IBOutlet weak var isOff: UISwitch!
     @IBOutlet weak var countBtn: UILabel!
     @IBOutlet weak var countSwitch: UILabel!
     
+    @IBOutlet weak var imgPicker1: UIPickerView!
+    @IBOutlet weak var imgPicker2: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        isBgOn = UIImage(named: "gromitBg2.jpg")
-        isBgOff = UIImage(named: "gromitnotBg2.jpg")
+        
+        for i in 0..<MAX_ARRAY_NUM {
+            let image1 = UIImage(named: gromitArray[i])
+            let image2 = UIImage(named: feathersArray[i])
+            
+            imageArray1.append(image1)
+            imageArray2.append(image2)
+        }
+        
+        isBgOn = UIImage(named: "imageArray1[0]")
+        isBgOff = UIImage(named: "imageArray2[0]")
         
         imgView.image = isBgOn
         updateCountLabels()
@@ -52,10 +68,65 @@ class SwitchViewController: UIViewController {
     }
     
     @IBAction func isOffImage(_ sender: UISwitch) {
-        // 삼항 연산자로 이미지 설정을 간결하게 처리
-        imgView.image = sender.isOn ? isBgOn : isBgOff
+        if sender.isOn {
+            // 스위치가 on이면 gromit picker에서 선택된 행의 이미지를 가져옴
+            let selectedRow = imgPicker1.selectedRow(inComponent: 0)
+            imgView.image = imageArray1[selectedRow]
+            self.view.backgroundColor = UIColor.white
+        } else {
+            // 스위치가 off이면 feathers picker에서 선택된 행의 이미지를 가져옴
+            let selectedRow = imgPicker2.selectedRow(inComponent: 0)
+            imgView.image = imageArray2[selectedRow]
+            self.view.backgroundColor = UIColor.black
+        }
+        
         inputSwitch += 1
         updateCountLabels()
     }
+    
+    // 피커뷰 개수 (컴포넌트 수는 동일하게 유지)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUM
+    }
+
+    // 피커뷰의 각 행 높이 설정
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKERVIEW_HEIGHT
+    }
+
+    // 피커뷰 행의 개수 설정
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == imgPicker1 {
+            return imageArray1.count
+        } else {
+            return imageArray2.count
+        }
+    }
+
+    // 피커뷰의 각 행에 표시할 뷰 설정
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image: UIImage?
+        if pickerView == imgPicker1 {
+            image = imageArray1[row]
+        } else {
+            image = imageArray2[row]
+        }
+        
+        let pickerImageView = UIImageView(image: image)
+        pickerImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        return pickerImageView
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 스위치 상태에 따라, 사용자가 선택한 피커뷰의 이미지를 imgView에 업데이트
+        if pickerView == imgPicker1 && isOff.isOn {
+            // 스위치가 on일 때, gromit picker 선택
+            imgView.image = imageArray1[row]
+        } else if pickerView == imgPicker2 && !isOff.isOn {
+            // 스위치가 off일 때, feathers picker 선택
+            imgView.image = imageArray2[row]
+        }
+    }
 }
+
 
